97c88ac6b3eb6838aa3927c852f0e6a52fbcc8db
MrSmoer <66489839+MrSmoer@users.noreply.github.com>
2023-11-29 21:02:23 +0100

    fix a few formatting errors

--- descramble.py
+++ descramble.py
@@ -3,7 +3,8 @@ import re
 
 def read_wordlist(lettercount):
     "opens a wordlist with the number of words"
-    with open("wordlists/"+str(lettercount)+".txt", "r", encoding='ascii') as f:
+    with open("wordlists/"+str(lettercount)+".txt",
+              "r", encoding='ascii') as f:
         b = f.read()
         return re.split(r'[\n,|]', b)
 
@@ -12,31 +13,44 @@ wordlistnumbers = [1, 2, 5]
 WORDLISTS = {}
 for i in wordlistnumbers:
     WORDLISTS[i] = read_wordlist(i)
+""" email me i smell
+   ex3
+   mx3
+   ax1
+   ix2
+   lx3
+   sx1
+"""
+
 
 LETTERS = {
-    "Diving Weight": ["weight", "d", "b", "t", "lead"], # l
-    "Mug": ["mug", "cup"], # m e*m*ail
-    "Hammer Head": ["hammer", "a", "iron"], # i
-    "Reverse Mousetrap": ["trap", "mousetrap", "reverse"], # m
-    "Lamp": ["lamp"], # l
-    "Apple": ["apple"], # a
-    "Egg": ["egg"], # e
-    "Egg2": ["egg"], # e
-    "Saw": ["saw", "tennon saw", "h"], # s *s*mell
-    "Lawnmower": ["lawnmower", "mower", "engine", "spinny-thingy"], # e
-    "Mallet": ["mallet", "hammerhead", "s"], # -> m
-    "Icecream": ["icecream", "v-the brand of icecream?"], # -> i
+    "Diving Weight": ["weight", "d", "b", "t", "lead"],             # l 
+    "Mug": ["mug", "cup"],                                          # m 2
+    "Hammer Head": ["hammer", "a", "iron"],                         # i 4
+    "Reverse Mousetrap": ["trap", "mousetrap", "reverse",  "lure"],  # m 6
+    "Lamp": ["lamp"],                                               # l 5
+    "Apple": ["apple"],                                             # a 3
+    "Egg": ["egg"],                                                 # e 1
+    "Egg2": ["egg"],                                                # e 7
+    "Saw": ["saw", "tennon saw", "h"],                              # s
+    "Lawnmower": ["lawnmower", "mower", "engine", "spinny-thingy"],  # l
+    "Mallet": ["mallet", "hammerhead"],                             # m 
+    "Icecream": ["icecream", "v-the brand of icecream?"],           # i 8
     # "Cage": ["cage", "aviary", "birdcage"],
-    "Money": ["money"], # -> m
+    "Money": ["money"],                                             # m 
 }
 
-predefined_pattern = ["e", "m", "a", "i", "l", " ", "m", "e", " ", "i", " ", "_", "_", "_", "_", "_"]
+predefined_pattern = ["e", "m", "a", "i", "l", " ", "m", "e", " ", "i", " ",
+                      "_", "_", "_", "_", "_"]
 
 
-def clean_one_predefined(pat: list[str] | (list[tuple[str, int] | str]), duck_letters: dict[str, list[str]]):
-    """ generates all possibilites for the letters to fulfill the given pattern, and appends them to the global precleaned_patterns
+def clean_one_predefined(pat: list[str] | (list[tuple[str, int] | str]),
+                         duck_letters: dict[str, list[str]]):
+    """ generates all possibilites for the letters to fulfill the given
+    pattern, and appends them to the global precleaned_patterns
     this function is recursive
-    a letter is dirty if it is predefined, but not predefined as a tuple -> just a string
+    a letter is dirty if it is predefined, but not predefined as
+    a tuple -> just a string
     """
     unchosen_dirty_letters = 0
     for index_in_pattern, letter_to_replace in enumerate(pat):
@@ -56,9 +70,11 @@ def clean_one_predefined(pat: list[str] | (list[tuple[str, int] | str]), duck_le
                         # copy current pattern for next depth
                         newLetters = duck_letters.copy()
                         newPattern = pat.copy()
-                        # replace the letter_to_replace with the chosen tuple from duckletters
-                        newPattern[index_in_pattern] = (duck_letter, index_in_subletters)
-                        # remove the chosen  duckletter, it cannot be used twice
+                        # replace the letter_to_replace with the chosen tuple
+                        # from duckletters
+                        newPattern[index_in_pattern] = (duck_letter,
+                                                        index_in_subletters)
+                        # remove the chosen  duckletter it cannot be used twice
                         newLetters.pop(duck_letter)
                         clean_one_predefined(newPattern, newLetters)
 
@@ -66,12 +82,15 @@ def clean_one_predefined(pat: list[str] | (list[tuple[str, int] | str]), duck_le
     if unchosen_dirty_letters < 1:
         PRECLEANED_PATTERNS.append((pat, duck_letters))
         if len(PRECLEANED_PATTERNS) % 100000 == 0:
-            print(f"{len(PRECLEANED_PATTERNS)}")  # f"asdf{len(PRECLEANED_PATTERNS)})
+            print(f"{len(PRECLEANED_PATTERNS)}")
+            # f"asdf{len(PRECLEANED_PATTERNS)})
 
 
 def render_pattern(pat: list[tuple[str, int]] | list[str]):
-    """ build as string from a given pattern, a pattern being either a list of strings or a tuple
-    the tuple would be containg the index of the duckwordword in the words and the index of the actual word in a duckword """
+    """ build as string from a given pattern, a pattern being either a list of
+    strings or a tuple
+    the tuple would be containg the index of the duckwordword in the words and
+    the index of the actual word in a duckword """
     result_str = ""
     for symbol_tuple in pat:
         if not isinstance(symbol_tuple, tuple):
@@ -80,7 +99,7 @@ def render_pattern(pat: list[tuple[str, int]] | list[str]):
 
         if symbol_tuple[1] is None:
             continue
-        
+
         result_str += LETTERS[symbol_tuple[0]][symbol_tuple[1]][0]
     return result_str
 
@@ -98,11 +117,12 @@ def is_all_words(s):
 
 
 def check_is_word(word: str):
-    "checks if word could be in wordlist, returns false if already no possibility left"
+    """checks if word could be in wordlist, returns false if already no
+    possibility left"""
     # this could be faster with biary search over sorted list
     wordlength = len(word)
     is_in_wordlist = False
-    
+
     word = word.replace("_", ".")
     for s in WORDLISTS[wordlength]:
         if re.match(word, s) is not None:
@@ -110,8 +130,10 @@ def check_is_word(word: str):
     return is_in_wordlist
 
 
-def begin_from_next_free_position(pattern: list[str] | (list[tuple[str, int] | str]), letters: dict[str, list[str]], skip=0):
-    """ populates the free _ with the leftover letters 
+def begin_from_next_free_position(pattern: list[str] |
+                                  (list[tuple[str, int] | str]),
+                                  letters: dict[str, list[str]], skip=0):
+    """ populates the free _ with the leftover letters
         recursive function
     """
     # begin at index skip
@@ -127,16 +149,18 @@ def begin_from_next_free_position(pattern: list[str] | (list[tuple[str, int] | s
                     s = render_pattern(new_pattern)
                     if is_all_words(s):
                         if len(pattern) <= k+1 or pattern[k+1] == " ":
-                            # print(s)
+                            print(s)
                             POSSIBLE_SOULTIONS.add(s)
                             # print(newPattern)
-                        
-                        begin_from_next_free_position(new_pattern, new_letters, k)
+
+                        begin_from_next_free_position(new_pattern, new_letters,
+                                                      k)
             break
-    
+
 
 PRECLEANED_PATTERNS = []
 POSSIBLE_SOULTIONS = set()
+
 
 def main():
     letters = LETTERS.copy()
@@ -146,13 +170,15 @@ def main():
     print("Egg" in letters.keys())
     print(render_pattern(predefined_pattern))
     clean_one_predefined(predefined_pattern, letters)
-    print("All possible patterns for your input computed ("+str(len(PRECLEANED_PATTERNS))+")")
+    print("All possible patterns for your input computed (" +
+          str(len(PRECLEANED_PATTERNS))+")")
     for p in PRECLEANED_PATTERNS:
         # breakpoint()
         begin_from_next_free_position(p[0], p[1])
 
         for p in POSSIBLE_SOULTIONS:
             print(p)
-    
+
+
 if __name__ == '__main__':
     main()
